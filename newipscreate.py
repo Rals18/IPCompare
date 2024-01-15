d555f99a19a8e0b50df62453ea4c3ed464cccb49
Rals18 <72716083+Rals18@users.noreply.github.com>
2024-01-14 20:20:54 -0500

    newipscreate.py

---
+++ newipscreate.py
@@ -0,0 +1,19 @@
+import ipaddress
+#create new_ips and new_ip text files using new_ip_list text file
+#creates the numerical value ranges for IPs in order to easily compare
+def cidr_range_to_numbers(cidr_range):
+    ip_network = ipaddress.ip_network(cidr_range, strict=False)
+    start_ip = int(ip_network.network_address)
+    end_ip = int(ip_network.broadcast_address)
+    return start_ip, end_ip
+
+ip = open("new_ip_list.txt",'r')
+cidr_rangee = ip.read()
+cidr_ranges = cidr_rangee.split("\n")
+ip.close()
+
+ips = open("new_ips.txt",'w')
+for cidr_range in cidr_ranges:
+    start_ip, end_ip = cidr_range_to_numbers(cidr_range)
+    print ("{}".format(cidr_range), "{}".format(start_ip), "{}".format(end_ip), file=ips)
+ips.close()
